20f3adc98f208a8eeaffbe8f7327b8f528a21fd3
beasiteri <siteribea@gmail.com>
2026-05-22 15:14:27 +0200

    Create the PageWrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import PageWrapper from './components/PageWrapper';
 
 const App = () => {
   return (
-    <>
+    <PageWrapper>
       <Header />
-    </>
+    </PageWrapper>
   );
 };
 
