c0369f419638cc63cd911fcab0c496e0f90aca5f
beasiteri <siteribea@gmail.com>
2026-05-22 15:17:52 +0200

    Create the PageWrapper component

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,6 +1,6 @@
 const PageWrapper = ({ children }) => {
   return (
-    <main className="min-h-dvh bg-gradient-to-b from-cyan-200 to-white to-[60vh]">{children}</main>
+    <div className="min-h-dvh bg-gradient-to-b from-cyan-200 to-white to-[60vh]">{children}</div>
   );
 };
 
