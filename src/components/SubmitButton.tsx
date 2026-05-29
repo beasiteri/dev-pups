42fb6663bd0d1d907a0cbef70c07a51c21f027cf
beasiteri <siteribea@gmail.com>
2026-05-29 21:53:15 +0200

    Disable submit button on status pending

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -4,8 +4,9 @@ const SubmitButton = () => {
   const status = useFormStatus();
   return (
     <button
-      className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
+      className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none disabled:bg-slate-200 disabled:cursor-not-allowed"
       type="submit"
+      disabled={status.pending}
     >
       {status.pending ? 'Adding puppy...' : 'Add puppy'}
     </button>
