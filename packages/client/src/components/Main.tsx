f9f77aeec68117609e4ec355fc24307b89e454a6
beasiteri <siteribea@gmail.com>
2026-06-07 13:30:53 +0200

    Create the PuppiesList component with the puppies

--- packages/client/src/components/Main.tsx
+++ packages/client/src/components/Main.tsx
@@ -1,12 +1,16 @@
+import PuppiesList from './PuppiesList';
 import Search from './Search';
 import ShortList from './ShortList';
 
 const Main = () => {
    return (
-      <div className="mt-24 grid gap-8 sm:grid-cols-2">
-         <Search />
-         <ShortList />
-      </div>
+      <main>
+         <div className="mt-24 grid gap-8 sm:grid-cols-2">
+            <Search />
+            <ShortList />
+         </div>
+         <PuppiesList />
+      </main>
    );
 };
 
