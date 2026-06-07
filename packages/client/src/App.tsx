353a515d6e0ca57a7dbbc263085c3d48812c297e
beasiteri <siteribea@gmail.com>
2026-06-07 13:19:29 +0200

    Create the Main component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Container from './components/Container';
 import Header from './components/Header';
+import Main from './components/Main';
 import PageWrapper from './components/PageWrapper';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <PageWrapper>
          <Container>
             <Header />
+            <Main />
          </Container>
       </PageWrapper>
    );
---
+++ packages/client/src/components/Main.tsx
@@ -0,0 +1,5 @@
+const Main = () => {
+   return <div className="mt-24 grid gap-8 sm:grid-cols-2"></div>;
+};
+
+export default Main;
