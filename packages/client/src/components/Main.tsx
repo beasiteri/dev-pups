e50c7dbd900a0f7a0ebedf0293e40d281a2a1ab2
beasiteri <siteribea@gmail.com>
2026-06-09 23:09:45 +0200

    Replace useEffect with use for data fetching

--- packages/client/src/components/Main.tsx
+++ packages/client/src/components/Main.tsx
@@ -1,42 +1,21 @@
-import { useEffect, useState } from 'react';
-import { LoaderCircle } from 'lucide-react';
+import { use, useState } from 'react';
+
 import NewPuppyForm from './NewPuppyForm';
 import PuppiesList from './PuppiesList';
 import Search from './Search';
 import ShortList from './ShortList';
-import type { Puppy } from '../types/puppy';
+
 import { LikedContext } from '../context/likedContext';
+import type { Puppy } from '../types/puppy';
+import getPuppies from '../queries';
+
+const puppyPromise = getPuppies();
 
 const Main = () => {
+   const apiPuppies = use(puppyPromise);
    const [liked, setLiked] = useState<Puppy['_id'][]>([]);
    const [searchQuery, setSearchQuery] = useState<string>('');
-   const [puppies, setPuppies] = useState<Puppy[]>([]);
-   const [isLoading, setIsLoading] = useState(false);
-   const [error, setError] = useState<string>('');
-
-   useEffect(() => {
-      async function getPuppies() {
-         setIsLoading(true);
-         try {
-            const response = await fetch('/puppies');
-            if (!response.ok) {
-               const errorData = await response.json();
-               setError(errorData.message || 'Something went wrong');
-               throw errorData;
-            }
-            const data = await response.json();
-            console.log(data);
-            setPuppies(data);
-         } catch (error) {
-            console.log(error);
-            setError('Failed to load puppies');
-         } finally {
-            setIsLoading(false);
-         }
-      }
-
-      getPuppies();
-   }, []);
+   const [puppies, setPuppies] = useState<Puppy[]>(apiPuppies);
 
    return (
       <main>
@@ -48,11 +27,6 @@ const Main = () => {
                />
                <ShortList puppies={puppies} />
             </div>
-
-            {isLoading && (
-               <LoaderCircle className="animate-spin stroke-slate-300" />
-            )}
-            {error && <p className="text-red-500">{error}</p>}
 
             <PuppiesList puppies={puppies} searchQuery={searchQuery} />
          </LikedContext.Provider>
