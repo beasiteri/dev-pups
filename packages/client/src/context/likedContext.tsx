9e52e70aa7bf827226a18f8ebea68f284b9a3154
beasiteri <siteribea@gmail.com>
2026-06-09 23:00:09 +0200

    Modify the id for LikedContext

--- packages/client/src/context/likedContext.tsx
+++ packages/client/src/context/likedContext.tsx
@@ -2,8 +2,8 @@ import { createContext, use, type Dispatch, type SetStateAction } from 'react';
 import type { Puppy } from '../types/puppy';
 
 export const LikedContext = createContext<{
-   liked: Puppy['id'][];
-   setLiked: Dispatch<SetStateAction<Puppy['id'][]>>;
+   liked: Puppy['_id'][];
+   setLiked: Dispatch<SetStateAction<Puppy['_id'][]>>;
 }>(null!);
 
 export function useLiked() {
